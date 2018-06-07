c327fad7b53f6f453702fc3bb4e089eecfba1c08
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-07 18:32:40 -0400

    Created a div with classes that will allow me to add a picture behind the quote.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
+import './index.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>
-          Hello Torb.
-        </h1>
+      <div className = "main-header">
+       <div className = "quoteDiv">
+        <h1>Build 'em Up, Break 'em Down.</h1>
+       </div>
       </div>
     );
   }

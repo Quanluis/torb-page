30a2f1af59e2566bde313924699701803deda7fa
Luis Garzon <luisgarzon90@yahoo.com>
2018-06-08 21:15:29 -0400

    Fixed issue with text getting cut off on mobile viewing and cleaned up code with prettier.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
-import './index.css'
+import React, { Component } from "react";
+import "./index.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-      <div className = "main-header">
-       <div className = "quoteDiv">
-        <h1>"Build 'em Up, Break 'em Down."</h1>
-       </div>
-      </div>
-      <h2>The last line of defense.</h2>
-      <p>Torbjörn Lindholm (called "Torby" by his friends) is one of the greatest weaponsmiths and engineers in the world. He has a low opinion of omnics, believing them to be incapable of empathy, instead acting purely on logic, and disagrees with giving them the same rights as humans.</p>
+        <div className="main-header">
+          <div className="quoteDiv">
+            <h1>"Build 'em Up, Break 'em Down."</h1>
+          </div>
+        </div>
+        <h2>The last line of defense.</h2>
+        <p>
+          Torbjörn Lindholm (called "Torby" by his friends) is one of the
+          greatest weaponsmiths and engineers in the world. He has a low opinion
+          of omnics, believing them to be incapable of empathy, instead acting
+          purely on logic, and disagrees with giving them the same rights as
+          humans.
+        </p>
       </div>
     );
   }
